e4c1826fb3da6453eaf820b641993e3a3b5cee23
Vijay Kesanakurthi <n170467@rguktn.ac.in>
2024-04-19 12:34:49 +0530

    processing

---
+++ creative-coding/try_processing/src/main/java/TryProcessing.java
@@ -0,0 +1,36 @@
+import processing.core.PApplet;
+
+public class TryProcessing extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("TryProcessing", args);
+    }
+
+    private static  final int WIDTH = 1080;
+    private static final int HEIGHT=720;
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void setup() {
+        background(255);
+    }
+
+    @Override
+    public void draw() {
+            drawRowOfCircles();
+    }
+
+    private void drawCircle(int x, int y){
+        ellipse(x, y, 50, 50);
+        fill(200, 0, 100);
+    }
+
+    private void drawRowOfCircles(){
+        for (int i = 1; i < 10; i++) {
+            drawCircle(50+ i * 100, 50 );
+        }
+    }
+}
