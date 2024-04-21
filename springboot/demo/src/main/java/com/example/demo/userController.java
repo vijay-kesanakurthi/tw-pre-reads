ff460c2dff162a0fc5edd8565b01e36dcd975644
Vijay Kesanakurthi <n170467@rguktn.ac.in>
2024-04-21 11:43:01 +0530

    Making parameters as optional/required

--- springboot/demo/src/main/java/com/example/demo/userController.java
+++ springboot/demo/src/main/java/com/example/demo/userController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.*;
 public class userController {
 
     @GetMapping(path="/{userId}")
-    public String getUser(@PathVariable String userId, @RequestParam(value="page") int page){
-        return "The user id is: " + userId+ " and the page is: " + page;
+    public String getUser(@PathVariable String userId, @RequestParam(value="page") int page, @RequestParam(value="limit",defaultValue = "1",required = false) int limit){
+        return "The user id is: " + userId+ " and the page is: " + page + " and the limit is: "+ limit;
     }
 
     @PostMapping
