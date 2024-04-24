48015041591cfa5614e4cd742dcd9cc1bd868610
Vijay Kesanakurthi <n170467@rguktn.ac.in>
2024-04-24 11:08:20 +0530

    Returning custom error messages as object

--- springboot/demo/src/main/java/com/example/demo/controllers/UserController.java
+++ springboot/demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -29,6 +29,8 @@ public class UserController {
     @GetMapping(path="/{userId}",produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
 
     public ResponseEntity<UserRest> getUser(@PathVariable String userId){
+        String first = null;
+        var length = first.length();
         if( users.containsKey(userId)){
             return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
         }else{
---
+++ springboot/demo/src/main/java/com/example/demo/exceptions/AppExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.demo.exceptions;
+
+import com.example.demo.models.ErrorMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request){
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if (errorMessageDescription == null) errorMessageDescription = ex.toString();
+        ErrorMessage errorMessage = new ErrorMessage();
+        errorMessage.setMessage(errorMessageDescription);
+        errorMessage.setTimestamp(new java.util.Date());
+
+        return new ResponseEntity<>(errorMessage,new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
+}
